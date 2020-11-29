68686718be29f77be701e044c5266ab55a3c49fe
Aleksandr Dankovtsev <a.dankovtsev@mail.ru>
2020-11-29 13:35:16 +0300

    add object request for save

---
+++ src/Request/V1/Content/Save.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Alexander1000/Clients/Content/Request/V1/Content;
+
+use NetworkTransport;
+
+class Save extends NetworkTransport\Http\Request\Data
+{
+    public __construct(
+        ?int $id,
+        string $title,
+        string $text,
+        int $userId,
+    ) {
+        parent::__construct(
+            '/v1/content/save',
+            'POST',
+            [
+                'Content-Type' => 'application/json',
+            ],
+            []
+        );
+        $this->data = [
+            'id' => $id,
+            'title' => $title,
+            'text' => $text,
+            'userId' => $userId,
+        ];
+    }
+}
+
